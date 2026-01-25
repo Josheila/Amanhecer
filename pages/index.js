41a2a278a193964f837c4d5618089876aca98814
Sheila Zhai <sheilazhai@neverland.local>
2026-01-25 23:18:49 +0800

    xxx

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,18 @@
-export default function Home() { return <h1>Amanhecer 🌅</h1> }
+import Link from 'next/link';
+import Image from 'next/image';
+import avatar from '../public/avatar.jpg';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <Image src={avatar} alt="Avatar" width={150} height={150} />
+      <h1>Sheila</h1>
+
+      <nav style={{ marginTop: '30px' }}>
+        <Link href="/now"><a style={{ margin: '0 15px' }}>Now</a></Link>
+        <Link href="/posts"><a style={{ margin: '0 15px' }}>Posts</a></Link>
+        <Link href="/about"><a style={{ margin: '0 15px' }}>About Me</a></Link>
+      </nav>
+    </div>
+  );
+}
